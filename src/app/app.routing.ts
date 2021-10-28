6a21a63059c20fbc9ca4697e568fcf5e9576b42a
Palaciodiego008 <palaciodiego099@gmail.com>
2021-10-27 20:08:40 -0500

    listar clientes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from '@angular/core';
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { LoginComponent } from "./components/login/login.component";
+import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
 
 import { AdminGuard } from "./guard/admin.guard";
 
 const appRoute: Routes = [
     {path: '', component: LoginComponent},
+    {path: 'panel', children:[
+      {path: 'clientes', component: IndexClienteComponent, canActivate: [AdminGuard]}
+    ]},
     {path: 'inicio', component: InicioComponent, canActivate: [AdminGuard]}
 ];
 
